ba54dc8b8f01325c60535b5a51a5cb442191fafb
mlclmtan <52649203+mlclmtan@users.noreply.github.com>
2020-04-23 11:11:32 +0800

    Update db.php

--- db.php
+++ db.php
@@ -18,8 +18,6 @@
 
     function refresh(){
         global $conn;
-        // $stmt = $conn->prepare('SELECT `vue`,`react`,`angular`, DATE_FORMAT(`date`, "%m/%e") d, `date` x FROM `web` ORDER BY x ASC
-        // ');
         $stmt = $conn->prepare('SELECT `vue`,`react`,`angular`, DATE_FORMAT(`date`, "%m/%e") d, `date` x FROM `web` ORDER BY x ASC
         ');
         if (!$stmt->execute()) {
@@ -53,9 +51,7 @@
                                             FROM   `web` 
                                             WHERE  CURDATE() = 
                                                     Date_format(`date`, "%Y/%m/%e")) ');
-
-
-        // INSERT INTO `web` (`vue`,`react`,`angular`) VALUES (?,?,?)
+                                                    
         $stmt->bind_param("iii", $vue[1], $react[1], $angular[1]);
         if (!$stmt->execute()) {
             $err = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
